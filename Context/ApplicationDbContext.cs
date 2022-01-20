cb9cb350502cbdc439fb413d3436692b97d2878c
HSGamer <huynhqtienvtag@gmail.com>
2022-01-20 20:27:02 +0700

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _4kTiles_Backend.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    }
+}
---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4kTiles_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string PrintHello()
+        {
+            return "Hello World";
+        }
+    }
+}
