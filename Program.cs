64a8546bc2e958165bc7309b8fd214c49df505de
HSGamer <huynhqtienvtag@gmail.com>
2022-01-26 10:17:26 +0700

    force use swagger

--- Program.cs
+++ Program.cs
@@ -61,11 +61,11 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
