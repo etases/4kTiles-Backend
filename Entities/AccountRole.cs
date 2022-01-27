c8e5b74df11efe6d9b6392d8235f87435aef9af8
Trainex <50549524+tunbk1002@users.noreply.github.com>
2022-01-27 17:47:13 +0700

    scafford dbcontext (#3)
    Signed-off-by: tulx <lexuantu1002@gmail.com>

---
+++ Entities/AccountRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4kTiles_Backend.Entities
+{
+    public partial class AccountRole
+    {
+        public int ArId { get; set; }
+        public int AccountId { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Account Account { get; set; } = null!;
+        public virtual Role Role { get; set; } = null!;
+    }
+}
---
+++ Entities/Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4kTiles_Backend.Entities
+{
+    public partial class Song
+    {
+        public Song()
+        {
+            AccountSongs = new HashSet<AccountSong>();
+            SongReports = new HashSet<SongReport>();
+            SongTags = new HashSet<SongTag>();
+        }
+
+        public int SongId { get; set; }
+        public string SongName { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public int Bpm { get; set; }
+        public string Notes { get; set; } = null!;
+        public DateTime ReleaseDate { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public string? DeletedReason { get; set; }
+
+        public virtual ICollection<AccountSong> AccountSongs { get; set; }
+        public virtual ICollection<SongReport> SongReports { get; set; }
+        public virtual ICollection<SongTag> SongTags { get; set; }
+    }
+}
