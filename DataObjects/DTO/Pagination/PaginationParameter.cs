dd3be1bfb5937fbf6f9229bd0a19eeeff1ce0530
HSGamer <huynhqtienvtag@gmail.com>
2022-02-02 19:16:59 +0700

    Add PaginationHelper

---
+++ DataObjects/DTO/Pagination/PaginationParameter.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4kTiles_Backend.DataObjects.DTO.Pagination;
+
+public record PaginationParameter
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
