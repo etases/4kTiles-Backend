7093b16f19b407e415c0229e0434aa39b953509c
tulx <lexuantu1002@gmail.com>
2022-02-07 10:47:05 +0700

    re scaffold dbcontext
    Signed-off-by: tulx <lexuantu1002@gmail.com>

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace _4kTiles_Backend.Entities
 {
+    [Table("Role")]
     public partial class Role
     {
         public Role()
@@ -10,9 +14,14 @@ namespace _4kTiles_Backend.Entities
             AccountRoles = new HashSet<AccountRole>();
         }
 
+        [Key]
+        [Column("roleId")]
         public int RoleId { get; set; }
+        [Column("roleName")]
+        [StringLength(50)]
         public string RoleName { get; set; } = null!;
 
+        [InverseProperty(nameof(AccountRole.Role))]
         public virtual ICollection<AccountRole> AccountRoles { get; set; }
     }
 }
