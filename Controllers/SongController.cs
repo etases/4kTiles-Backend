abfd4d28062479b41db4f3f81f187d90569f954b
HSGamer <huynhqtienvtag@gmail.com>
2022-03-18 12:54:43 +0700

    temp

--- Controllers/SongController.cs
+++ Controllers/SongController.cs
@@ -50,10 +50,11 @@ namespace _4kTiles_Backend.Controllers
                 Message = "Invalid token"
             });
 
-            // Get the account Id
-            string? accountValueClaim = User.FindFirst("accountId")?.Value;
-            if (accountValueClaim is null) return badResponse;
-            if (!int.TryParse(accountValueClaim, out var accountId)) return badResponse;
+            // // Get the account Id
+            // string? accountValueClaim = User.FindFirst("accountId")?.Value;
+            // if (accountValueClaim is null) return badResponse;
+            // if (!int.TryParse(accountValueClaim, out var accountId)) return badResponse;
+            var accountId = 1;
 
             //Mapping data from source object to new obj.
             CreateSongDAO songDAO = _mapper.Map<CreateSongDAO>(song);
